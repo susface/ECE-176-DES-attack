3e5f622b2f6d77e3f18178ecc96918088c1fb98b
Jordan Reynolds <jdreynolds586@gmail.com>
2026-05-02 21:41:03 -0700

    Made improvements to sideBFSM and tb. To-do: Side B top module

--- side B/sideBFSM.v
+++ side B/sideBFSM.v
@@ -1,66 +1,111 @@
+// This module is authored by Jordan David Reynolds
 
 module sideBFSM (
-    input clk, reset, start,
+    input clk, reset_n, start,
     input [63:0] receivedPlaintext,
     input [63:0] receivedCiphertext,
-    input [63:0] startingKey,
-    input [63:0] endingKey,
     output reg match,
     output [63:0] key
 );
 
-    reg         rst, start_internal;
+    localparam IDLE = 3'b000;
+    localparam RESET_PULSE = 3'b001;
+    localparam ENCRYPTION_PULSE = 3'b010;
+    localparam ENCRYPTING = 3'b011;
+    localparam ENCRYPTION_DONE = 3'b100;
+    localparam MATCH_FOUND = 3'b101;
+
+
+    parameter startingKey = 64'h0000000000000000,
+    endingKey = 64'hFFFFFFFFFFFFFFFF;
+
+    reg [2:0] currentState;
+    reg [2:0] nextState;
+    reg         rst_n, start_internal;
     wire [63:0] ciphertext;
     wire        done;
     reg [55:0] key_int;
     // Skip redundant parity bits
     assign key = {key_int[55:49], 1'b0, key_int[48:42], 1'b0, key_int[41:35], 1'b0, key_int[34:28], 1'b0, key_int[27:21], 1'b0, key_int[20:14], 1'b0, key_int[13:7], 1'b0, key_int[6:0], 1'b0};
 
-    reg encryptionInProgress;
 
     des_datapath desEngine (
-        .clk(clk), .rst_n(rst), .start(start_internal),
+        .clk(clk), .rst_n(rst_n), .start(start_internal),
         .decrypt(1'b0),
         .plaintext(receivedPlaintext), .key(key),
         .ciphertext(ciphertext), .done(done)
     );
 
-    always@(posedge clk) begin
-        if(reset) begin
+    always @(posedge clk or negedge reset_n) begin
+        //$display(currentState);
+        if (!reset_n) begin 
+            currentState <= IDLE;
             key_int <= {startingKey[63:57], startingKey[55:49], startingKey[47:41], startingKey[39:33], startingKey[31:25], startingKey[23:17], startingKey[15:9], startingKey[7:1]};
-            rst <= 0;
-            encryptionInProgress <= 0;
-            start_internal <= 0;
-            match <= 0;
         end
-
         else begin
-            if(start && !encryptionInProgress && rst) begin
-                start_internal <= 1;
-                encryptionInProgress <= 1;
-            end
-            else if(encryptionInProgress) begin
-                start_internal <= 0;
-                if (done) begin
-                    if(ciphertext == receivedCiphertext) begin
-                        match <= 1;
-                        encryptionInProgress <= 0;
-                    end
-                    else begin
-                        if (key_int <= {endingKey[63:57], endingKey[55:49], endingKey[47:41], endingKey[39:33], endingKey[31:25], endingKey[23:17], endingKey[15:9], endingKey[7:1]}) begin
-                            key_int <= key_int + 1;
-                            rst <= 0;
-                            encryptionInProgress <= 0;
-                        end
-                    end
-                end else begin
-                    rst <= 1;
+        currentState <= nextState;
+        if (currentState == ENCRYPTION_DONE && ciphertext != receivedCiphertext) begin
+                if (key_int < {endingKey[63:57], endingKey[55:49], endingKey[47:41], 
+                               endingKey[39:33], endingKey[31:25], endingKey[23:17], 
+                               endingKey[15:9], endingKey[7:1]}) begin
+                    key_int <= key_int + 1;
                 end
-            end else begin
-                rst <= 1;
             end
-
         end
     end
 
+    always @(*) begin
+        nextState = currentState;
+        start_internal = 0;
+        rst_n = 1;
+        match = 0;
+
+        case (currentState)
+        IDLE : begin
+            if (start) nextState = RESET_PULSE;
+            else nextState = IDLE;
+        end
+
+        RESET_PULSE : begin
+            rst_n = 0;
+            nextState = ENCRYPTION_PULSE;
+        end
+
+        ENCRYPTION_PULSE : begin
+            rst_n = 1;
+            start_internal = 1;
+            nextState = ENCRYPTING;
+        end
+
+        ENCRYPTING : begin
+            start_internal = 0;
+            if (done) begin
+                nextState = ENCRYPTION_DONE;
+            end
+            else begin
+                nextState = ENCRYPTING;
+            end
+        end
+
+        ENCRYPTION_DONE : begin
+            //$displayh(key);
+            if (ciphertext == receivedCiphertext) begin
+                nextState = MATCH_FOUND;
+            end
+            else begin
+                nextState = RESET_PULSE;
+            end
+            end
+
+        MATCH_FOUND : begin
+            match = 1;
+            nextState = MATCH_FOUND;
+        end
+
+        default : begin
+            nextState = IDLE;
+        end
+
+        endcase
+    end
 endmodule
--- side B/sideBFSM_tb.v
+++ side B/sideBFSM_tb.v
@@ -1,18 +1,19 @@
+// This module is authored by Jordan David Reynolds
+
 `timescale 1ns / 1ps
 module sideB_tb;
 
-    reg clk, reset, start;
+    reg clk, reset_n, start;
     reg [63:0] targetPlaintext;
     reg [63:0] receivedCiphertext;
     wire [63:0] cipherText;
     wire match;
     wire [63:0] key;
 
-    sideBFSM uut(
-        .clk(clk), .reset(reset), .start(start),
+    sideBFSM #(.startingKey(64'h0000000000000000), .endingKey(64'hFFFFFFFFFFFFFFFF)) uut (
+        .clk(clk), .reset_n(reset_n), .start(start),
         .receivedPlaintext(targetPlaintext), .receivedCiphertext(receivedCiphertext),
-        .match(match), .key(key),
-        .startingKey(64'h0000000000000000), .endingKey(64'hFFFFFFFFFFFFFFF)
+        .match(match), .key(key)
     );
 
     reg des_reset, des_start;
@@ -27,21 +28,36 @@ module sideB_tb;
     always #10 clk = ~clk;
 
     initial begin
+        // Initial values, reset DES Core and SideBFSM
         clk = 0;
-        reset = 1;
+        reset_n = 0;
         des_reset = 0;
-        #20;
         start = 0;
+        des_start = 0;
         targetPlaintext = 64'h4772616465204121; // "Grade A!"
-        #10;
+        
+        #20;
+        
+        // Release DES Core reset and pulse start
+        des_reset = 1; 
         des_start = 1;
-        des_reset = 1;
-        wait(des_done);
+        #20; 
+        des_start = 0; 
+        
+        // Wait for DES to finish encrypting the target
+        @(des_done);
         receivedCiphertext = cipherText;
-        reset = 0;
+        
+        // Release FSM reset and pulse start to begin brute force
+        reset_n = 1;
         #20;
         start = 1;
-        wait(match);
+        #20;
+        start = 0;
+        
+        // Wait for the FSM to find the key
+        @(match);
+        
         // Result should be 0x0000000000003222
         $display("Found key: 0x%h", key);
         $finish;
